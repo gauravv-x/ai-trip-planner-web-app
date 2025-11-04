7843903ad48e42d1751ff40ccb90ea2e2085ccf8
Gaurav Akabari <gauravakabari22@gnu.ac.in>
2025-11-05 00:36:06 +0530

    Change export to const for OpenAI instance

--- app/api/aimodel/route.tsx
+++ app/api/aimodel/route.tsx
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import OpenAI from 'openai';
 import { aj } from "../arcjet/route";
   
- export const openai = new OpenAI({
+const openai = new OpenAI({
   baseURL: 'https://openrouter.ai/api/v1',
   apiKey: process.env.OPENROUTER_API_KEY,
  });
